3f98e499a30585dca0959743200390a9c712086b
ganguri <ganguri@parti.xyz>
2017-05-24 18:33:07 +0900

    특정 장소에서 일어난 성차별 사례만 보여줍니다

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -4,4 +4,19 @@ class Feedback < ApplicationRecord
   def all_cases
     [case1, case2, case3, case4, case5].reject { |c| c.blank? }
   end
+
+  def find_cases(spot)
+
+    cases = [case1, case2, case3, case4, case5]
+    case_spots = [case1_spot, case2_spot, case3_spot, case4_spot, case5_spot]
+    results = []
+
+    case_spots.each_with_index do |s, index|
+      if s == spot
+        results.push(cases[index])
+      end
+    end
+
+    return results
+  end
 end
