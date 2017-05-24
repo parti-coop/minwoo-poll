2d45cbe9e4e85a6dbb6aa6c0d36673b159278c34
ganguri <ganguri@parti.xyz>
2017-05-24 20:17:58 +0900

    장소 버튼을  눌렀을 때 해당 장소에서 일어난 성차별 사례가 나타납니다

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,20 +1,23 @@
 class Feedback < ApplicationRecord
   #attr_accessor :created_at, :age, :region, :case1, :case1_spot, :case2, :case2_spot, :case3, :case3_spot, :case4, :case4_spot, :case5, :case5_spot
 
-  def all_cases
-    [case1, case2, case3, case4, case5].reject { |c| c.blank? }
-  end
+  #def all_cases
+  #  [case1, case2, case3, case4, case5].reject { |c| c.blank? }
+  #end
 
   def find_cases(spot)
-
     cases = [case1, case2, case3, case4, case5]
     case_spots = [case1_spot, case2_spot, case3_spot, case4_spot, case5_spot]
     results = []
 
-    case_spots.each_with_index do |s, index|
-      if s == spot
-        results.push(cases[index])
+    if spot != 0
+      case_spots.each_with_index do |s, index|
+        if s == spot
+          results.push(cases[index])
+        end
       end
+    else
+      results = cases.reject{ |c| c.blank? }
     end
 
     return results
